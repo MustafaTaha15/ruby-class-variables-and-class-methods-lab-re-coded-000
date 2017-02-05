d7c634a5b28fc823bf63a8ac33d8fc0167651417
Mustafa Taha <mtmax1994@gmail.com>
2017-02-05 07:22:41 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+
+  attr_accessor :name, :artists, :gener
+  @@count
+  @@geners=[]
+
+  def initialize (:name, :artists, :gener)
+    @name=name
+    @artists=artists
+    @gener=gener
+
+  end
+
+
+
+end
